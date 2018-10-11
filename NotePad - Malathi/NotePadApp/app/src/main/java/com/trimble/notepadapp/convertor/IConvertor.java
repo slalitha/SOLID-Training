71c64b7101dedb321740dcd7081d39527d3df88a
EU\vkumarm <vimal_kumarm@trimble.com>
2018-10-11 16:44:03 +0530

    Delete Malathi project from my branch

--- NotePad - Malathi/NotePadApp/app/src/main/java/com/trimble/notepadapp/convertor/IConvertor.java
+++
@@ -1,9 +0,0 @@
-package com.trimble.notepadapp.convertor;
-
-/**
- * Created by mbommi on 10/8/2018.
- */
-
-public interface IConvertor {
-    void convert(String text);
-}
--- NotePad - Malathi/NotePadApp/app/src/main/java/com/trimble/notepadapp/convertor/PDFConvertor.java
+++
@@ -1,8 +0,0 @@
-package com.trimble.notepadapp.convertor;
-
-/**
- * Created by mbommi on 10/8/2018.
- */
-
-public class PDFConvertor extends IConvertor {
-}
