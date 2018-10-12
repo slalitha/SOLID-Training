d9c8f13eea140093e1f8efdb4ab5c869d0f2fff0
EU\bsivaku <bavarna_sivakumar@trimble.com>
2018-10-12 11:41:31 +0530

    removing unused classes

--- Notepad_bavarna/src/application/Billing/BillingWithSubs.java
+++
@@ -1,9 +0,0 @@
-
-public class BillingWithSubs extends Billing{
-	
-	int subs;
-	
-	public double calculateBill() {
-		return cost+subs;
-	}
-}
--- Notepad_bavarna/src/application/Billing/IBilling.java
+++
@@ -1,6 +0,0 @@
-
-public interface IBilling {
-	
-	double calculateBill();
-
-}
