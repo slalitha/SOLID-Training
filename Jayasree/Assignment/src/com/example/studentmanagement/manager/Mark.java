17961332918c4c0d96abe26bb28cb3132fbf303c
Jayasree Pattabiraman <jpattab@eu.trimblecorp.net>
2018-10-15 11:01:51 +0530

    Remove

--- Jayasree/Assignment/src/com/example/studentmanagement/manager/Mark.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentmanagement.manager;
-
-import java.util.List;
-
-public interface Mark {
-	public List<Integer> getTotalMarks(List<Integer> marks,int attendance,int internal);
-}
--- Jayasree/Assignment/src/com/example/studentmanagement/manager/StudentResult.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentmanagement.manager;
-
-import java.util.List;
-
-public abstract class StudentResult {
-	abstract String getStudentResult(List<Integer> totalmarks);
-}
