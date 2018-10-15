8ffdb525b0cd2f64b20694f0f70a001ce509ec3d
schandh <shrutisaraswati_chandhirasekaran@trimble.com>
2018-10-15 11:29:13 +0530

    Added Folder

---
+++ StudentMarks/StudentMarks/Subject/Subject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentMarks
+{
+    class Subject : SubjectMarks
+    {
+        string subjectName;
+
+        public Subject()
+        {
+        }
+
+        public Subject(string name)
+        {
+            subjectName = name;
+        }
+        public string getSubjectName()
+        {
+            return subjectName;
+        }
+        public void setSubjectName (string name)
+        {
+            subjectName = name;
+        }
+    }
+}
